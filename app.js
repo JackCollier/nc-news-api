720b555dee709d428edf61be1321414cfd7d17d0
JacvaSipt <steamcow12@hotmail.com>
2023-06-26 12:10:53 +0100

    create route for getApi

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { getTopics } = require("./controllers/api-controllers");
+const { getTopics, getApi } = require("./controllers/api-controllers");
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/", getApi);
 
 module.exports = app;
