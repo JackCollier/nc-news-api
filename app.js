2359c1bd9f74c4d5d29363aca5278f8580d2bcbb
JacvaSipt <steamcow12@hotmail.com>
2023-06-26 16:30:57 +0100

    improve error middleware to ensure more specific error handling

--- app.js
+++ app.js
@@ -12,24 +12,18 @@ app.get("/api/", getApi);
 
 app.get("/api/articles/:article_id", getArticleById);
 
-app.use("/*", (err, req, res, next) => {
-  res.status(404).send({ msg: "Not Found" });
-});
-
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
-    res.status(400).send({ msg: "Bad Request" });
-  } else {
-    next(err);
-  }
-});
-
-app.use((err, req, res, next) => {
-  if (err.msg) {
+    res.status(400).send({ msg: "Bad request" });
+  } else if (err.status && err.msg) {
     res.status(err.status).send(err.msg);
   } else {
     next(err);
   }
+});
+
+app.use((err, req, res, next) => {
+  res.status(404).send({ msg: "Not Found" });
 });
 
 module.exports = app;
