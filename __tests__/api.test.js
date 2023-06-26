563160c146adaa24d9f8bbcd353618b51b6f1a6b
JacvaSipt <steamcow12@hotmail.com>
2023-06-26 15:34:16 +0100

    add test to check object comes back from API correctly

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -56,6 +56,26 @@ describe("GET /api/articles/:article_id", () => {
   test("should respond with a 200 status", () => {
     return request(app).get("/api/articles/1").expect(200);
   });
+  test("should respond with an object article containing the passed id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
+        expect(body).toHaveProperty("article", expect.any(Object));
+        expect(article).toMatchObject({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
 });
 
 describe("Error testing", () => {
