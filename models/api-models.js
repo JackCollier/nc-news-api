ed4db8b558fd3a5c4aa4bc08e3a0bdcd00723d07
JacvaSipt <steamcow12@hotmail.com>
2023-06-26 15:36:22 +0100

    create function to select article by id and sanitize request params

--- models/api-models.js
+++ models/api-models.js
@@ -12,3 +12,11 @@ exports.getApiEndpoints = () => {
     return data;
   });
 };
+
+exports.selectArticleById = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then(({ rows }) => {
+      return rows[0];
+    });
+};
