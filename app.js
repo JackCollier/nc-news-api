ff05e0b7c3f229920fa5fb93ec1cd8dab3e7dd3c
JacvaSipt <steamcow12@hotmail.com>
2023-06-26 15:21:40 +0100

    add routing for /api/articles/:article_id end point

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { getTopics, getApi } = require("./controllers/api-controllers");
+const {
+  getTopics,
+  getApi,
+  getArticleById,
+} = require("./controllers/api-controllers");
 const app = express();
 
 app.get("/api/topics", getTopics);
 
 app.get("/api/", getApi);
+
+app.get("/api/articles/:article_id", getArticleById);
+
 app.use("/*", (err, req, res, next) => {
   res.status(404).send({ msg: "Not Found" });
 });
