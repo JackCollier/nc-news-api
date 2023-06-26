e39c7325ae2c4bd8484ae13897975e90aafdb076
JacvaSipt <steamcow12@hotmail.com>
2023-06-26 16:31:59 +0100

    add catch block for error handling

--- controllers/api-controllers.js
+++ controllers/api-controllers.js
@@ -26,7 +26,11 @@ exports.getApi = (req, res, next) => {
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
-  selectArticleById(article_id).then((article) => {
-    res.status(200).send({ article });
-  });
+  selectArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
