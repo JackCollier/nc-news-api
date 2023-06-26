1e03e1a2b2c02ce83a1e04cd4ce141b2e764dec4
JacvaSipt <steamcow12@hotmail.com>
2023-06-26 11:43:58 +0100

    add functionality to allow testing for a key holding an array

--- __tests__/getApiTopics.test.js
+++ __tests__/getApiTopics.test.js
@@ -1,8 +1,22 @@
 const app = require("../app");
 const request = require("supertest");
+const data = require("../db/data/test-data/index");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+
+beforeEach(() => seed(data));
+afterAll(() => db.end());
 
 describe("GET /api/topics", () => {
   test("should respond with a 200 status", () => {
     return request(app).get("/api/topics").expect(200);
+  });
+  test("should respond with an array of topic objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body).toHaveProperty("topics", expect.any(Array));
+      });
   });
 });
