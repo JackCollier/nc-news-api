b347f09011e036edadb7fded10bec5d3841f4b85
JacvaSipt <steamcow12@hotmail.com>
2023-07-20 15:01:49 +0100

    update delete query in comment model to more accurately  check for deletion

--- models/api-models.js
+++ models/api-models.js
@@ -173,7 +173,15 @@ exports.selectUsers = () => {
 };
 
 exports.deleteComment = (comment_id) => {
-  return db.query(`DELETE FROM comments WHERE comment_id = $1;`, [comment_id]);
+  return db
+    .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [
+      comment_id,
+    ])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Resource not found" });
+      }
+    });
 };
 
 exports.deleteArticle = (article_id) => {
